0e8377a0eeca870df560bd67a70080a3d4596ff2
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-12-22 18:38:37 +0100

    Basic mechanics (for PC)

---
+++ Unity Project/Confitados/Assets/Scripts/InputPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputPlayer : MonoBehaviour
+{
+    public float inputX { get; private set; }
+    public float inputZ { get; private set; }
+    public bool dashInput { get; private set; }
+    public bool superDashInput { get; private set; }
+
+    [HideInInspector]
+    public Vector3 faceDirection;
+
+    void Start()
+    {
+        faceDirection = new Vector3(0.0f, 0.0f, 1.0f);
+    }
+
+    private void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+        dashInput = Input.GetButtonDown("Dash");
+        superDashInput = Input.GetButtonDown("SuperDash");
+
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            faceDirection.x = inputX;
+            faceDirection.z = inputZ;
+        }
+    }
+}
---
+++ Unity Project/Confitados/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(InputPlayer))]
+public class PlayerController : MonoBehaviour
+{
+    //Movement
+    public float moveSpeed;
+    private Vector3 movement;
+    private Vector3 dashMovement;
+
+    //Rotation
+    public float rotationSpeed;
+    private Quaternion targetRotation;
+
+    //Dashes
+    public float dashSpeed;
+    public float superDashSpeed;
+
+    private bool inputControl = true;
+    private bool dashing = false;
+    public float dashTime;
+    public float superDashTime;
+
+    private float currentCooldown;
+    public float dashAgainCooldown;
+    public float superDashAgainCooldown;
+
+    //References
+    private Rigidbody rb;
+    private InputPlayer input;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        input = GetComponent<InputPlayer>();
+    }
+
+    private void Update()
+    {
+        if (inputControl)
+        {
+            movement = new Vector3(input.inputX * moveSpeed, rb.velocity.y, input.inputZ * moveSpeed);
+            targetRotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(input.faceDirection), rotationSpeed * Time.deltaTime);
+        }
+
+        if (!dashing)
+        {
+            if (input.dashInput)
+            {
+                Dash(dashTime, dashAgainCooldown);
+                dashMovement = input.faceDirection * dashSpeed;
+            }
+            else if (input.superDashInput)
+            {
+                Dash(superDashTime, superDashAgainCooldown);
+                dashMovement = input.faceDirection * superDashSpeed;
+            }
+        }
+        else
+            DashCountdown();
+    }
+
+    private void FixedUpdate()
+    {
+        if (inputControl)
+        {
+            rb.velocity = movement;
+            transform.rotation = targetRotation;
+        }
+        else
+            rb.velocity = dashMovement;
+    }
+
+    private void Dash(float time, float coolDown)
+    {
+        currentCooldown = coolDown;
+        dashing = true;
+        inputControl = false;
+        StartCoroutine(InputControlBack(time));
+    }
+
+    private void DashCountdown()
+    {
+        if (currentCooldown >= 0)
+            currentCooldown -= Time.deltaTime;
+        else
+            dashing = false;
+    }
+
+    IEnumerator InputControlBack(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        inputControl = true;
+    }
+}
