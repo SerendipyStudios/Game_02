b6f4ce2b0da4f8b35a6d8c612e3bfb0372a2a9bf
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2020-12-24 11:47:35 +0100

    Face direction input improvement
    - The face direction has to be ckecked depending on the raw input, not basing on the true input itself

--- Unity Project/Confitados/Assets/Scripts/InputPlayer.cs
+++ Unity Project/Confitados/Assets/Scripts/InputPlayer.cs
@@ -10,14 +10,14 @@ public class InputPlayer : MonoBehaviour
     public Platforms platform;
 
     public Joystick joystick;
-    public float inputX { get; private set; }
-    public float inputZ { get; private set; }
+    public float inputX; //{ get; private set; }
+    public float inputZ; //{ get; private set; }
     public bool dashInput { get; private set; }
     public bool superDashInput { get; private set; }
 
     private bool dashMobile, superDashMobile; //Mobile phone dash inputs
 
-[HideInInspector]
+    //[HideInInspector]
     public Vector3 faceDirection;
 
     void Start()
@@ -33,25 +33,52 @@ public class InputPlayer : MonoBehaviour
         switch (platform)
         {
             case Platforms.PC:
+                //Inputs check
                 inputX = Input.GetAxis("Horizontal");
                 inputZ = Input.GetAxis("Vertical");
                 dashInput = Input.GetButtonDown("Dash");
                 superDashInput = Input.GetButtonDown("SuperDash");
+
+                //Face direction update
+                if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+                {
+                    faceDirection.x = Input.GetAxisRaw("Horizontal");
+                    faceDirection.z = Input.GetAxisRaw("Vertical");
+                }
+
                 break;
 
             case Platforms.Mobile:
+                //Inputs check
                 inputX = joystick.Horizontal;
                 inputZ = joystick.Vertical;
                 dashInput = dashMobile;
                 superDashInput = superDashMobile;
+
+                //Face direction update
+                joystick.SnapX = true;
+                joystick.SnapY = true;
+                if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+                {
+                    faceDirection.x = joystick.Horizontal;
+                    faceDirection.z = joystick.Vertical;
+                    joystick.SnapX = false;
+                    joystick.SnapY = false;
+                }
                 break;
         }
 
-        if (inputX != 0 || inputZ != 0)
-        {
-            faceDirection.x = inputX;
-            faceDirection.z = inputZ;
-        }
+
+    }
+
+    private float FaceSideRaw(float value)
+    {
+        if (value > 0)
+            return 1;
+        else if (value < 0)
+            return -1;
+        else
+            return 0;
     }
 
     IEnumerator UseDashMobile()
