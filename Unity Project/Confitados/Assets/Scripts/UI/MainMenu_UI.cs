3c53c0eb39f4306433f35d0e4105c2e5a63f3566
Julenjus <julenjus.creative@gmail.com>
2020-12-24 15:05:08 +0100

    Provisional Screen Structure

---
+++ Unity Project/Confitados/Assets/Scripts/UI/MainMenu_UI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu_UI : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+}
