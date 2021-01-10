7a1934622be94a98a8c2ad334be4d92478dde177
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2021-01-10 13:44:52 +0100

    Scenes changes

--- Unity Project/Confitados/Assets/Scripts/Managers/SkinsManagement.cs
+++ Unity Project/Confitados/Assets/Scripts/Managers/SkinsManagement.cs
@@ -9,33 +9,43 @@ public class SkinsManagement : MonoBehaviour
     public Sprite[] playerSkinsPreviews;
     public Image playerSkinImage;
 
-    [HideInInspector] public int skinIndex = 0; //[HERE] this index must be sent to game manager (How?)
+    public int skinIndex = 0; //[HERE] this index must be sent to game manager (How?)
 
     private void Start()
     {
         UpdateSkinPreview();
     }
 
-    private void Update()
+    public void NextSkin()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            skinIndex = ++skinIndex < playerSkins.Length ? skinIndex++ : 0;
-            UpdateSkinPreview();
-        }
-        else if (Input.GetMouseButtonDown(2))
-        {
-            Instantiate(playerSkins[skinIndex], new Vector3(7.729721f, 0.21f, 1f), Quaternion.identity, transform);
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            skinIndex = --skinIndex >= 0 ? skinIndex-- : playerSkins.Length - 1;
-            UpdateSkinPreview();
-        }
+        skinIndex = ++skinIndex < playerSkins.Length ? skinIndex++ : 0;
+        if (skinIndex > 5)
+            NextAvailableSkin();
+        UpdateSkinPreview();
+    }
+
+    public void PreviousSkin()
+    {
+        skinIndex = --skinIndex >= 0 ? skinIndex-- : playerSkins.Length - 1;
+        if (skinIndex < 0)
+            PreviousAvailableSkin();
+        UpdateSkinPreview();
     }
 
     public void UpdateSkinPreview()
     {
         playerSkinImage.sprite = playerSkinsPreviews[skinIndex];
+    }
+
+    private void NextAvailableSkin()
+    {
+        while(PlayerPrefs.GetInt("PaidSkin_" + skinIndex, 0) == 0 || skinIndex > 0)
+            skinIndex = ++skinIndex < playerSkins.Length ? skinIndex++ : 0;
+    }
+
+    private void PreviousAvailableSkin()
+    {
+        while (PlayerPrefs.GetInt("PaidSkin_" + skinIndex, 0) == 0 || skinIndex > 5)
+            skinIndex = --skinIndex >= 0 ? skinIndex-- : playerSkins.Length - 1;
     }
 }
