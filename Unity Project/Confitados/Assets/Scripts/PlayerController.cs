1b9dc0b3f4bf49ac755d1590eadf3c6284145659
David-Gonzalez-Bella <davidgonzalezbella99@gmail.com>
2021-01-10 19:45:48 +0100

    Instantiate joystick only on mobile first try

--- Unity Project/Confitados/Assets/Scripts/PlayerController.cs
+++ Unity Project/Confitados/Assets/Scripts/PlayerController.cs
@@ -128,8 +128,11 @@ public class PlayerController : MonoBehaviourPunCallbacks, IOnEventCallback
         //Instantiate Controls
         if (PlayerInputUIPrefab != null)
         {
-            PlayerControlUI inputUiGo = Instantiate(PlayerInputUIPrefab);
-            input.Initialize(inputUiGo.GetComponent<PlayerControlUI>());
+            if (SystemInfo.deviceType == DeviceType.Handheld)
+            {
+                PlayerControlUI inputUiGo = Instantiate(PlayerInputUIPrefab);
+                input.Initialize(inputUiGo.GetComponent<PlayerControlUI>());
+            }
         }
         else
         {
